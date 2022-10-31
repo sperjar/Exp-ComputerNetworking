324021c3100ccb516ec1c16fda761a236330f3be
sperjar <65524316+sperjar@users.noreply.github.com>
2022-11-01 00:20:30 +0800

    feat: add link to home

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,14 @@
 <template>
   <nav text-xl mt-6 inline-flex gap-2>
+    <router-link to="/">
+      <div inline-block icon-btn i-carbon-home />
+    </router-link>
     <button icon-btn @click="toggleDark()">
-      <div dark:i-carbon-moon i-carbon-sun />
+      <div inline-block dark:i-carbon-moon i-carbon-sun />
     </button>
-
     <a
-      icon-btn i-carbon-logo-github
-      rel="noreferrer"
-      href="https://github.com/sperjar/Exp-ComputerNetworking"
-      target="_blank"
-      title="GitHub"
+      icon-btn i-carbon-logo-github rel="noreferrer" href="https://github.com/sperjar/Exp-ComputerNetworking"
+      target="_blank" title="GitHub"
     />
   </nav>
 </template>

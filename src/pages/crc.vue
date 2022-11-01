578571083174f49e111b1d41f0ce166c329f5fda
sperjar <65524316+sperjar@users.noreply.github.com>
2022-11-01 14:18:49 +0800

    feat: crc encoding

--- src/pages/crc.vue
+++ src/pages/crc.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable no-alert -->
 <script lang="ts" setup>
 enum ValidResult {
   Valid,
@@ -10,6 +11,92 @@ const rawData = ref('')
 const rawPolyData = ref('')
 const encodingData = ref('')
 const decodingData = ref('')
+
+function crcEncoding(rawData: string, polyData: string) {
+  const checkNum = polyData.length - 1 // length of check digit (CRC)
+  const polyBitsArray = polyData.split('').map(x => parseInt(x))
+  const data = rawData.split('').map(x => parseInt(x)).concat(new Array(checkNum).fill(0))
+  const bitsXOR = (a: number[], b: number[]) => a.map((x, i) => x ^ b[i])
+  for (let i = 0; i < rawData.length; i++) {
+    if (data[i] === 0)
+      continue
+    data.splice(i, polyData.length, ...bitsXOR(data.slice(i, i + polyData.length), polyBitsArray))
+  }
+  return rawData + data.slice(-checkNum).join('')
+}
+
+function crcValidating(data: string, polyData: string): boolean {
+  const checkNum = polyData.length - 1 // length of check digit (CRC)
+  const polyBitsArray = polyData.split('').map(x => parseInt(x))
+  const bitsXOR = (a: number[], b: number[]) => a.map((x, i) => x ^ b[i])
+  const dataBitsArray = data.split('').map(x => parseInt(x))
+  for (let i = 0; i < dataBitsArray.length - checkNum; i++) {
+    if (dataBitsArray[i] === 0)
+      continue
+    dataBitsArray.splice(i, polyData.length, ...bitsXOR(dataBitsArray.slice(i, i + polyData.length), polyBitsArray))
+  }
+  return dataBitsArray.slice(-polyData.length).every(x => x === 0)
+}
+
+function crcDecoding(data: string, polyData: string): string {
+  return data.slice(0, -polyData.length + 1)
+}
+
+function encode(): void {
+  // check input
+  if (rawData.value.length === 0) {
+    alert('请输入原始数据')
+    throw new Error('原始数据输入有误：无原始数据')
+  }
+  else if (!rawData.value.match(/^[01]+$/)) {
+    alert('原始数据输入有误：请输入仅包含`0`和`1`的字符串')
+    throw new Error('原始数据输入有误：原始数据格式错误')
+  }
+  else if (rawPolyData.value.length === 0) {
+    alert('请输入生成多项式')
+    throw new Error('生成多项式输入有误：无生成多项式')
+  }
+  else if (!rawPolyData.value.match(/1[0|1]*1|1/)) {
+    alert('生成多项式输入有误：请输入仅包含`0`和`1`的字符串，且第一位和最后一位必须为`1`')
+    throw new Error('生成多项式输入有误：生成多项式格式错误')
+  }
+
+  encodingData.value = crcEncoding(rawData.value, rawPolyData.value)
+}
+
+function validate(): void {
+  // check input
+  if (encodingData.value.length === 0) {
+    validResult.value = ValidResult.Empty
+    alert('请输入编码后的数据')
+    throw new Error('编码后的数据输入有误：无编码后的数据')
+  }
+  else if (!encodingData.value.match(/^[01]+$/)) {
+    validResult.value = ValidResult.Empty
+    alert('编码后的数据输入有误：请输入仅包含`0`和`1`的字符串')
+    throw new Error('编码后的数据输入有误：编码后的数据格式错误')
+  }
+  else if (rawPolyData.value.length === 0) {
+    validResult.value = ValidResult.Empty
+    alert('请输入生成多项式')
+    throw new Error('生成多项式输入有误：无生成多项式')
+  }
+  else if (!rawPolyData.value.match(/1[0|1]*1|1/)) {
+    validResult.value = ValidResult.Empty
+    alert('生成多项式输入有误：请输入仅包含`0`和`1`的字符串，且第一位和最后一位必须为`1`')
+    throw new Error('生成多项式输入有误：生成多项式格式错误')
+  }
+
+  if (crcValidating(encodingData.value, rawPolyData.value))
+    validResult.value = ValidResult.Valid
+  else
+    validResult.value = ValidResult.Invalid
+}
+
+function decode(): void {
+  validate()
+  decodingData.value = crcDecoding(encodingData.value, rawPolyData.value)
+}
 </script>
 
 <template>
@@ -23,7 +110,7 @@ const decodingData = ref('')
     </div>
     <div class="item">
       <label for="poly_data">生成多项式</label>
-      <input id="poly_dta" v-model="rawPolyData" class="value" type="text">
+      <input id="poly_dta" v-model="rawPolyData" class="value" type="text" pattern="1[0|1]*1|1">
     </div>
     <div class="item">
       <label for="encoding_data">编码数据</label>
@@ -40,13 +127,13 @@ const decodingData = ref('')
     </div>
   </div>
   <div my-8>
-    <button>
+    <button @click="encode()">
       编码
     </button>
-    <button>
+    <button @click="validate()">
       校验
     </button>
-    <button>
+    <button @click="decode()">
       解码
     </button>
   </div>
